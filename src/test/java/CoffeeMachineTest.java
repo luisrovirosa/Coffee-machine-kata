dff2761b3e64fd799e0bdcf569a2176738e43af0
Luis Rovirosa <luisrovirosa@gmail.com>
2017-02-07 17:10:11 +0100

    added tests for chocolate

--- src/test/java/CoffeeMachineTest.java
+++ src/test/java/CoffeeMachineTest.java
@@ -10,6 +10,7 @@ public class CoffeeMachineTest {
 
     public static final double PRICE_OF_COFFEE = 0.6;
     public static final double PRICE_OF_TEA = 0.4;
+    public static final double PRICE_OF_CHOCOLATE = 0.5;
     private DrinkMaker drinkMaker;
 
     @Before
@@ -45,6 +46,21 @@ public class CoffeeMachineTest {
     @Test
     public void send_a_message_with_the_missing_amount_when_prepare_a_tea_without_enough_money() {
         verifyMessageIsSentWith(0.30, new Tea(0), "0,1 euros");
+    }
+
+    @Test
+    public void send_the_order_to_prepare_a_chocolate_when_there_is_enough_money() {
+        verifyServeOrder(PRICE_OF_CHOCOLATE, new Chocolate(0));
+    }
+
+    @Test
+    public void do_not_send_the_order_to_prepare_a_chocolate_when_there_is_no_enough_money() {
+        verifyDoNotServeOrder(PRICE_OF_CHOCOLATE - 0.1, new Chocolate(0));
+    }
+
+    @Test
+    public void send_a_message_with_the_missing_amount_when_prepare_a_chocolate_without_enough_money() {
+        verifyMessageIsSentWith(0.30, new Chocolate(0), "0,2 euros");
     }
 
     private void verifyServeOrder(double payedAmount, Order order) {
