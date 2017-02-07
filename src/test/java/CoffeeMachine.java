d9a6348319560ff22fecab7e2642f151b6999974
Luis Rovirosa <luisrovirosa@gmail.com>
2017-02-07 16:12:23 +0100

    refactor: simplify public method

--- src/test/java/CoffeeMachine.java
+++ src/test/java/CoffeeMachine.java
@@ -6,18 +6,14 @@ public class CoffeeMachine {
     }
 
     public void serve(Order order) {
-        if (order.numberOfSugars() == 0) {
-            driver.send("C::");
-        } else {
-            driver.send("C:" + numberOfSugars(order) + ":" + stickCommand(order));
-        }
+        driver.send("C:" + numberOfSugars(order) + ":" + stickCommand(order));
     }
 
     private String stickCommand(Order order) {
-        return "0";
+        return order.numberOfSugars() > 0 ? "0" : "";
     }
 
     private String numberOfSugars(Order order) {
-        return String.valueOf(order.numberOfSugars());
+        return order.numberOfSugars() > 0 ? String.valueOf(order.numberOfSugars()) : "";
     }
 }
