2400cb2998135433365ed6d280dd1e0213e25803
Luis Rovirosa <luisrovirosa@gmail.com>
2017-02-07 16:03:20 +0100

    refactor: inline variable

--- src/test/java/CoffeeMachineTest.java
+++ src/test/java/CoffeeMachineTest.java
@@ -6,33 +6,30 @@ import static org.mockito.Mockito.verify;
 public class CoffeeMachineTest {
     @Test
     public void should_send_the_order_to_serve_a_coffee_without_sugar(){
-        Order order = coffeeWithoutSugar();
         MachineDriver driver = mock(MachineDriver.class);
         CoffeeMachine machine = new CoffeeMachine(driver);
 
-        machine.serve(order);
+        machine.serve(coffeeWithoutSugar());
 
         verify(driver).send("C::");
     }
 
     @Test
     public void should_send_the_order_to_serve_a_coffee_with_one_sugar(){
-        Order order = coffeeWithSugar(1);
         MachineDriver driver = mock(MachineDriver.class);
         CoffeeMachine machine = new CoffeeMachine(driver);
 
-        machine.serve(order);
+        machine.serve(coffeeWithSugar(1));
 
         verify(driver).send("C:1:0");
     }
 
     @Test
     public void should_send_the_order_to_serve_a_coffee_with_two_sugar(){
-        Order order = coffeeWithSugar(2);
         MachineDriver driver = mock(MachineDriver.class);
         CoffeeMachine machine = new CoffeeMachine(driver);
 
-        machine.serve(order);
+        machine.serve(coffeeWithSugar(2));
 
         verify(driver).send("C:2:0");
     }
